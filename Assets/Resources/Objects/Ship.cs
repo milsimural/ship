f237b661cd52f7e3dfb34f2699fea9eb8f305952
milsimural <108474929+milsimural@users.noreply.github.com>
2024-03-25 07:23:58 +0300

    Add Physics
    None

--- Assets/Resources/Objects/Ship.cs
+++ Assets/Resources/Objects/Ship.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Ship : ScriptableObject
 {
     public string Name;
-    public string Descriptio;
-    public EnumShipTypes Type;
-    public float MaxSpeed;
-    public float MaxWeight;
-    public int CargoSlots;
+    public string Description; 
+    public EnumShipTypes Type; // Тип корабля, от него зависит бонусы корабля. Капитан может прокачивать навыки управления каждым типом кораблей.
+    public float MaxSpeed; // Максимальная скорость корабля 0 - 12, переключается по еденице. Все модификаторы к скорости добавляются к максимальному значению.  
+    public float MaxWeight; // Максимальный вес оборудования, груза
+    public int CargoSlots; // Кол-вол слотов трюма
 }
--- Assets/Scripts/Controllers/ShipController.cs
+++ Assets/Scripts/Controllers/ShipController.cs
@@ -13,6 +13,8 @@ public class ShipController : MonoBehaviour
     private float RotateSpeed = 0.1f;
     private Vector3 TargetVector; 
     [SerializeField] private Ship Ship;
+    [SerializeField]
+    private Rigidbody Rb;
     void Start()
     {
         InterfaseState = EnumInterfaceStates.OnShipControll;
@@ -27,17 +29,24 @@ public class ShipController : MonoBehaviour
             else InterfaseState = EnumInterfaceStates.OnShipControll;
         }
 
-        if(Input.GetKeyDown(KeyCode.W)) CurrentSpeed = Mathf.Clamp(++CurrentSpeed, 0, MaxSpeed) / 100f;
+        if(Input.GetKeyDown(KeyCode.W)) CurrentSpeed = Mathf.Clamp(++CurrentSpeed, 0, MaxSpeed);
 
-        if(Input.GetKeyDown(KeyCode.S)) CurrentSpeed = Mathf.Clamp(--CurrentSpeed, 0, MaxSpeed) / 100f;
+        if(Input.GetKeyDown(KeyCode.S)) CurrentSpeed = Mathf.Clamp(--CurrentSpeed, 0, MaxSpeed);
 
         if (Input.GetKey(KeyCode.A)) --Angle;
 
         if (Input.GetKey(KeyCode.D)) ++Angle;
 
         if(Angle != 0f) ShipRotate(Angle);
+    }
 
-        if(CurrentSpeed != 0f) MoveShip(CurrentSpeed);
+    private void FixedUpdate()
+    {
+        if(CurrentSpeed != 0)
+        {
+            Rb.MovePosition(Rb.position + (Vector3.forward * CurrentSpeed) / 100);
+        }
+        
     }
 
     private void ShipRotate(float angle)
@@ -45,9 +54,9 @@ public class ShipController : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), RotateSpeed * Time.deltaTime);
     }
 
-    private void MoveShip(float speed)
-    {
-        TargetVector = transform.position + transform.forward;
-        transform.position = Vector3.MoveTowards(transform.position, TargetVector, speed);
-    }
+    //private void MoveShip(float speed)
+    //{
+    //    TargetVector = transform.position + transform.forward;
+    //    transform.position = Vector3.MoveTowards(transform.position, TargetVector, speed);
+    //}
 }
