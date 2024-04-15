b5a09bb19d3e8725f2eff62577cfcdb967fae79d
milsimural <108474929+milsimural@users.noreply.github.com>
2024-04-15 18:16:58 +0300

    Rotate

--- Assets/Scripts/Controllers/ShipController.cs
+++ Assets/Scripts/Controllers/ShipController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
@@ -15,6 +16,10 @@ public class ShipController : MonoBehaviour
     [SerializeField] private Ship Ship;
     [SerializeField]
     private Rigidbody Rb;
+    [SerializeField]
+    private TextMeshPro Speed_text;
+    [SerializeField]
+    private TextMeshPro Angle_text;
     void Start()
     {
         InterfaseState = EnumInterfaceStates.OnShipControll;
@@ -33,18 +38,24 @@ public class ShipController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.S)) CurrentSpeed = Mathf.Clamp(--CurrentSpeed, 0, MaxSpeed);
 
-        if (Input.GetKey(KeyCode.A)) --Angle;
+        if (Input.GetKey(KeyCode.A)) Angle = Angle - 0.1f;
 
-        if (Input.GetKey(KeyCode.D)) ++Angle;
+        if (Input.GetKey(KeyCode.D)) Angle = Angle + 0.1f;
 
-        if(Angle != 0f) ShipRotate(Angle);
+        if (Angle != 0f)
+        {
+            ShipRotate(Angle);
+        }
+
+        Speed_text.text = $"Speed: {CurrentSpeed.ToString()}";
+        Angle_text.text = $"Angle: {Angle}";
     }
 
     private void FixedUpdate()
     {
         if(CurrentSpeed != 0)
         {
-            Rb.MovePosition(Rb.position + (Vector3.forward * CurrentSpeed) / 100);
+            Rb.MovePosition(Rb.position + (transform.forward * CurrentSpeed) / 100);
         }
         
     }
@@ -52,6 +63,15 @@ public class ShipController : MonoBehaviour
     private void ShipRotate(float angle)
     {
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), RotateSpeed * Time.deltaTime);
+        if(Angle > 0) {
+            Angle = Angle - 0.5f * Time.deltaTime;
+        }
+        else if(Angle < 0)
+        {
+            Angle = Angle + 0.5f * Time.deltaTime;
+        }
+        else if(Angle == 0) { Angle = 0; }
+        
     }
 
     //private void MoveShip(float speed)
